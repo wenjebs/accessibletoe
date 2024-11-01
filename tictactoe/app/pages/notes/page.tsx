b50c182cdc49e08e585da19e5b7f9a7ebba8a401
wenjebs <wenjieb2@gmail.com>
2024-11-02 01:11:50 +0800

    remove unused files

--- tictactoe/app/pages/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-// import { createClient } from "../../utils/supabase/server";
-
-// export default async function Notes() {
-//   const supabase = await createClient();
-//   const { data: notes } = await supabase.from("gamedata").select();
-
-//   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
-// }
